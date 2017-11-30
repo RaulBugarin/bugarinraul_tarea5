bc6f571bee8cbf41dd548d93ba10eaa474b6d8f7
Your Name <you@example.com>
2017-11-30 12:12:31 +0100

    primera versión

---
+++ js/calc.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('#boton').click(function(){   
+        var i=0;
+        var numero=$("#num").val();
+        var mult=$("#mult").val();
+
+        for (i; i< mult; i++) {
+            i*numero}
+
+            $("#mensajes").append()
+
+
+    });
+});
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    /*var nombre = document.getElementById('nombre').value;
+
+    for (var i = i) {
+    $('#tablaMultiplicar tr:last').after('<tr><td>' + nombre + '</td><td>' + (multiplicacio) + '</td>');
+    
+    
+    }
+} */   
